650c984b5d5d0768ac4e1987d037b609f3863e27
Kirk <kirkwon@gmail.com>
2025-11-18 22:34:23 -0800

    keystatic prod config

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,9 +1,14 @@
 import { config, fields, collection } from '@keystatic/core';
 
 export default config({
-    storage: {
-        kind: 'local',
-    },
+    storage: import.meta.env.PROD
+        ? {
+            kind: 'github',
+            repo: 'kirkwon/astrohost',
+        }
+        : {
+            kind: 'local',
+        },
     collections: {
         blog: collection({
             label: 'Blog',
